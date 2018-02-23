803f2efb8e4bc5a926c4ed09e19786124c7df691
pi <pi@nils-geis.de>
2018-02-23 21:36:23 +0100

    ..

--- weather-check.py
+++ weather-check.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import sys
 import json
+import requests
 
 if len(sys.argv) > 1:
 	INTERVAL = float(sys.argv[1]) * 60
@@ -10,6 +11,10 @@ else:
 	INTERVAL = 60 * 60
 
 DATA_FILE = "data/data.csv"
+
+# API
+API_KEY = open('API_KEY', 'r').read().strip()
+API_URL = "https://api.thingspeak.com/update.json"
 
 sensor = Adafruit_DHT.DHT22
 pin = 10
@@ -27,7 +32,13 @@ while True:
 
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
         print(str(now) + "," + str(temperature) + "," + str(humidity))
-		
+	r = requests.post(API_URL, headers={"THINGSPEAKAPIKEY": API_KEY}, params={'field2': temperature, 'field3': humidity})	
+	try:
+		r.raise_for_status()
+	except requests.exceptions.HTTPError as e:
+		print(e)
+			
+
 	with open(DATA_FILE, "a") as data_file:
 		data_file.write("\n" + str(now) + "," + str(temperature) + "," + str(humidity))
 
